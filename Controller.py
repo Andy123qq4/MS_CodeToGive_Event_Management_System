e1c0b786fed8a03dd2500ecfb3b74c88cd5fd01f
angusk701 <angusk701@gmail.com>
2024-08-25 14:06:19 +0800

    Angus: deploy test 2

--- Controller.py
+++ Controller.py
@@ -408,7 +408,7 @@ def unregister_from_event():
 
 
 # ==================Main=======================
-
+            
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
