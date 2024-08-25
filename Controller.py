1ea018ccf68e0653dcb1445fa2b3da12699f842a
Andy123qq4 <ksyung@connect.ust.hk>
2024-08-25 14:20:09 +0800

    chore: Update os

--- Controller.py
+++ Controller.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request, make_response
 from werkzeug.security import generate_password_hash, check_password_hash
+import os
 from flask_cors import CORS
 from datetime import datetime
 from pymongo import MongoClient
@@ -498,7 +499,7 @@ def get_users_calendar(user_id):
 
 
 # ==================Main=======================
-            
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
