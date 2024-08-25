7ed750348c8033b22e611bae051cf9d49eccfc9e
angusk701 <angusk701@gmail.com>
2024-08-25 22:42:39 +0800

    angus cahnge port

--- Controller.py
+++ Controller.py
@@ -771,4 +771,5 @@ def handle_send_message():
 # ==================Main=======================
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=8080, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
