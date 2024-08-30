036e36a5c82aca553e2d139e4c6e3693bd558af2
Andy123qq4 <ksyung@connect.ust.hk>
2024-08-30 16:11:08 +0800

    Cleanup backend folder

--- backend/whatsapp_reminder.py
+++ backend/whatsapp_reminder.py
@@ -10,12 +10,13 @@ db = client["MSCodeToGive"]
 users = db["users"]
 
 # Twilio setup (use your provided Twilio credentials)
-account_sid = 'AC563a1ebd79285971e2cc35686654f09b'  # Your Twilio Account SID
-auth_token = '6b1c03162dec073f144245f259194186'  # Your Twilio Auth Token
-TWILIO_WHATSAPP_NUMBER = 'whatsapp:+14155238886'  # Twilio Sandbox WhatsApp number
+account_sid = "AC563a1ebd79285971e2cc35686654f09b"  # Your Twilio Account SID
+auth_token = "6b1c03162dec073f144245f259194186"  # Your Twilio Auth Token
+TWILIO_WHATSAPP_NUMBER = "whatsapp:+14155238886"  # Twilio Sandbox WhatsApp number
 
 # Create Twilio client
 twilio_client = Client(account_sid, auth_token)
+
 
 def send_whatsapp_message(to_number, message_body):
     """
@@ -26,20 +27,19 @@ def send_whatsapp_message(to_number, message_body):
     """
     try:
         message = twilio_client.messages.create(
-            body=message_body,
-            from_=TWILIO_WHATSAPP_NUMBER,
-            to=to_number
+            body=message_body, from_=TWILIO_WHATSAPP_NUMBER, to=to_number
         )
         print(f"Message sent to {to_number}: {message.sid}")
     except Exception as e:
         print(f"Failed to send message to {to_number}: {e}")
+
 
 def main():
     # List of ObjectIDs to query
     object_ids = [
         ObjectId("66cab5a4ad8ae30434b9ac95"),
         ObjectId("66c9bd8643e62a1474adbbe6"),
-        ObjectId("66caf2dcd7fe88b53e58871c")
+        ObjectId("66caf2dcd7fe88b53e58871c"),
     ]
 
     # Query MongoDB to fetch users with specified ObjectIDs
@@ -57,12 +57,13 @@ def main():
 
     # Send WhatsApp messages to each user
     for user in registered_users:
-        country_code = user.get('country_code', '')
-        contact_number = user.get('contact_number', '')
+        country_code = user.get("country_code", "")
+        contact_number = user.get("contact_number", "")
 
         if country_code and contact_number:
-            phone_number = f'whatsapp:{country_code}{contact_number}'
+            phone_number = f"whatsapp:{country_code}{contact_number}"
             send_whatsapp_message(phone_number, sample_message)
+
 
 if __name__ == "__main__":
     main()
